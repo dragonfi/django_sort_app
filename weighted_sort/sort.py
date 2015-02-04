d462f4da9bacd425426c5cc5865382e65fe8eda7
Bodor Dávid Gábor <david.gabor.bodor@gmail.com>
2015-02-04 20:58:12 +0100

    Add docstring to sort

--- weighted_sort/sort.py
+++ weighted_sort/sort.py
@@ -1,4 +1,38 @@
 def sort(sequence, weights = None):
+    """Sorts a sequence of dictionaries, normalized and weighted by weight.
+
+    To get the correct sorting order, first normalize each attribute
+    (so that the lowest and highest values from each attribute count the same)
+    then do a weighted sum of them as usual.
+
+    This ensures that each attribute will have roughly the same contribution,
+    regardless of the choice of base unit.
+
+    Example:
+
+        Weights:
+            foo: 1
+            bar: 2
+            baz: -5
+
+        Objects:
+             id:    1      2       3
+            foo:   11    101       1
+            bar:   -3      5      13
+            baz:  0.2   10.2     3.2
+
+        After normalization:
+             id:    1      2       3
+            foo:  0.1    1.0     0.0
+            bar:  0.0    0.5     1.0
+            baz:  0.0    1.0     0.3
+
+        Weighted sum (applying 1, 2, -5):
+             id:    1      2       3
+           wsum:  0.1   -3.0     0.5
+
+        Final order: 2, 1, 3
+    """
     if len(sequence) == 0:
         return sequence
     if weights is None:
