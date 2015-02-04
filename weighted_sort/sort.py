caa21f0a2895c317589675f54701bee19848de77
Bodor Dávid Gábor <david.gabor.bodor@gmail.com>
2015-02-04 20:24:24 +0100

    Add normalization to sorting function

--- weighted_sort/sort.py
+++ weighted_sort/sort.py
@@ -1,12 +1,28 @@
 def sort(sequence, weights = None):
+    if len(sequence) == 0:
+        return sequence
     if weights is None:
         return sorted(sequence)
     return sorted(sequence, key=get_weight_function(sequence, weights))
 
 def get_weight_function(sequence, weights):
+    normalize = {}
+    for attr in weights:
+        seq = list(map(lambda x: x[attr], sequence))
+        normalize[attr] = get_normalize_function(seq)
+
     def weight_function(elem):
         total = 0
         for attr, weight in weights.items():
-            total += elem[attr] * weight
+            total += normalize[attr](elem[attr]) * weight
         return total
     return weight_function
+
+def get_normalize_function(sequence):
+    smallest = min(sequence)
+    largest = max(sequence)
+    offset = smallest
+    length = largest - smallest
+    def normalize(number):
+        return (number - offset) / length
+    return normalize
--- weighted_sort/test_sort.py
+++ weighted_sort/test_sort.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 
-from weighted_sort.sort import sort
+from weighted_sort.sort import sort, get_normalize_function
 
 
 class TestWeightedSorting(TestCase):
+    def test_sort_empty_sequence_returns_empty_sequence(self):
+        self.assertEqual(sort([]), [])
+
     def test_sort_return_one_element_array(self):
         e = {"id": 1, "foo": "something", "bar": "something else"}
         self.assertEqual(sort([e]), [e])
@@ -13,7 +16,7 @@ class TestWeightedSorting(TestCase):
         expected = [-3, 1, 3, 4, 5]
         self.assertEqual(sort(sequence), expected)
 
-    def test_sort_sorts__by_key(self):
+    def test_sort_sorts_by_key(self):
         sequence = [
             {"foo": 3, "bar": 2, "baz": 1},
             {"foo": 10, "bar": 0, "baz": 1},
@@ -30,4 +33,27 @@ class TestWeightedSorting(TestCase):
         ]
         expected = sorted(sequence, key=lambda e: e["foo"] + e["bar"]*2)
         self.assertEqual(sort(sequence, {"foo": 1, "bar": 2}), expected)
+
+    def test_sort_does_normalized_weighted_sort(self):
+        sequence = [
+            {"foo": 0, "bar": 2, "baz": 1},
+            {"foo": 10, "bar": 0, "baz": 1},
+            {"foo": 20, "bar": 1, "baz": 1},
+        ]
+        expected = sorted(sequence, key=lambda e: e["foo"]/10 + e["bar"]*2)
+        self.assertEqual(sort(sequence, {"foo": 1, "bar": 2}), expected)
+
+    def test_normalizer_function(self):
+        seq = [1, 5, 25]
+        normalize = get_normalize_function(seq)
+        self.assertEqual(normalize(seq[0]), 0)
+        self.assertEqual(normalize(5),  1 / 6)
+        self.assertEqual(normalize(seq[-1]), 1)
+
+    def test_normalizer_function_for_negative_values(self):
+        seq = [-10, 5, 10]
+        normalize = get_normalize_function(seq)
+        self.assertEqual(normalize(seq[0]), 0)
+        self.assertEqual(normalize(5),  15 / 20)
+        self.assertEqual(normalize(seq[-1]), 1)
 
